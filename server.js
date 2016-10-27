91f4fcba3353f0e175da247bdc170aeec526e333
Darshan <dave1089@live.com>
2016-10-27 19:53:45 +0000

    API Call working

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 var express = require("express")
 var app = express();
+var request = require('request');
 var mongo = require("mongodb").MongoClient
-var url = process.env.MONGOLAB_URI //'mongodb://localhost:27017/microservice'
+var url = process.env.MONGOLAB_URI // if its local c9 'mongodb://localhost:27017/microservice'
 var port = process.env.PORT || 8080;
 
 // app.use(express.static(__dirname + '/public'))
+app.get('/',function(req,res){
+    // var param = req.params[0]
+var options = {
+  url: 'https://api.imgur.com/3/gallery/search?q_any=cats',
+  headers: {
+    'User-Agent': 'request',
+    'Authorization': 'Client-ID 5e7a8b343b0ae8b',
 
-var responseData = ''
-app.get('/new/*', function (req, res){
-  var param = req.params[0]
-   
+  }
+}
+
+request(options, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    res.send(JSON.parse(body)) 
+         }
+    })
 })
-
-
 
 app.listen(port, function () {
   console.log('The app listening on port ' + port +  '!');
