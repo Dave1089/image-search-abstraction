6a2c66f8529a585e4d99a4e82a1fc046b4a94124
Darshan <dave1089@live.com>
2016-10-30 05:34:26 +0000

    Final Git commit

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ var express = require("express")
 var app = express();
 var request = require('request');
 var mongo = require("mongodb").MongoClient
-var link = process.env.MONGOLAB_URI // if its local c9 'mongodb://localhost:27017/microservice'
+var link = process.env.MONGOLAB_URI // if its local c9 'mongodb://localhost:27017/microservice'  && 
 var port = process.env.PORT || 8080;
 
-// app.use(express.static(__dirname + '/public'))
+//Fetching most recent searches
+app.get('/api/imagesearch/latest',function(req,res){
+  
+  mongo.connect(link,function(err,db){
+    if(err) throw (err)
+    var collection = db.collection('images')
+    collection.find({},{ _id: 0 }).sort([["_id", -1]]).limit(10).toArray(function(err, docu) {
+    if(err) throw (err)
+    res.send(docu)
+    
+      })
+     db.close()
+ })
+  
+})
+
+
+
 app.get('/api/imagesearch/:para',function(req,res){
    var param = req.params['para']
   var ofset = req.query['offset'] || 0 // shorthand for ternary check for undefined
@@ -16,11 +33,10 @@ app.get('/api/imagesearch/:para',function(req,res){
     'Ocp-Apim-Subscription-Key': '76784dbdc4cd408f8bede1ff03905d75',
   }
 }
-
+//Requesting Image API 
 request(options, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var ar = [],el,i;
-    // "use strict";
     var newObj = JSON.parse(body)
     newObj["value"].forEach(function(el,i){
               ar.push(el)
@@ -28,10 +44,27 @@ request(options, function (error, response, body) {
     var transfer = ar.map(function(a){
       return ({"Text" : a.name, "Image URL" : a.thumbnailUrl , "Page URL": a.hostPageDisplayUrl})
     })
+    
+    // Connecting and insrting to db
+  mongo.connect(link,function(err,db){
+    if(err) throw err;
+  var collection = db.collection('images')
+  var d = new Date()
+  var doc = {
+      "Search Term": param,
+      "When": d.toISOString()
+  }
+  collection.insert(doc,function(err,data){
+        if(err) throw err
+        db.close()
+        })
+    })    
+    // 
     res.send(transfer)
         }
     })
 })
+
 
 app.listen(port, function () {
   console.log('The app listening on port ' + port +  '!');
